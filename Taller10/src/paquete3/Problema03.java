76d76233f204300733888b3546780622f1af0d86
Carlos Mejia <cdmejia4@utpl.edu.ec>
2022-12-26 17:28:51 -0500

    subida media

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,40 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+    public static void main(String[] args) {
+      String[][] notasCualitativas = new String[3][3];
+double[][] notasCuantitativas = {{7.1, 8, 10}, {3, 7, 9}, {10, 9, 2.1}};
+
+for (int i = 0; i < notasCuantitativas.length; i++) {
+for (int j = 0; j < notasCuantitativas[i].length; j++) {
+if (notasCuantitativas[i][j] >= 0
+&& notasCuantitativas[i][j] <= 2.9) {
+notasCualitativas[i][j] = "insuficiente";
+} else if (notasCuantitativas[i][j] >= 3
+&& notasCuantitativas[i][j] <= 4.9) {
+notasCualitativas[i][j] = "regular";
+} else if (notasCuantitativas[i][j] >= 5
+&& notasCuantitativas[i][j] <= 7.9) {
+notasCualitativas[i][j] = "buena";
+} else if (notasCuantitativas[i][j] >= 8
+&& notasCuantitativas[i][j] <= 9.5) {
+notasCualitativas[i][j] = "muy buena";
+} else if (notasCuantitativas[i][j] >= 9.6
+&& notasCuantitativas[i][j] <= 10) {
+notasCualitativas[i][j] = "sobresaliente";
 }
+}
+}
+for (int i = 0; i < notasCuantitativas.length; i++) {
+for (int j = 0; j < notasCuantitativas[i].length; j++) {
+System.out.printf("%.2f = %s\n",
+notasCuantitativas[i][j],
+notasCualitativas[i][j]);
+}
+}
+
+}
+}  
+        
+        
+        
--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -4,11 +4,42 @@
  * and open the template in the editor.
  */
 package paquete4;
-
+import java.util.Scanner;
+import java.util.Locale;
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double[][] datos = new double[2][2];
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.printf("Ingrese el dato para el arreglo");
+                datos[i][j] = entrada.nextDouble();
+            }
+        }
+        System.out.println("El arreglo queda:");
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.printf("%.0f\t",
+                        datos[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
+
+
+    
+    
+    
+    
+    
+
